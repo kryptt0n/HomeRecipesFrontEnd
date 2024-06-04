d95b914fcf29381abb86815419a3ecc73b81349a
Vitaly Sukhinin <vitaliksuhinin99@gmail.com>
2024-06-03 20:55:21 -0400

    display products as cards on main page

--- src/components/ListDishes.jsx
+++ src/components/ListDishes.jsx
@@ -6,13 +6,14 @@ export default function ListDishes() {
 
     const [dishes, setDishes] = useState([]);
     const auth = useAuth();
+    const username = auth.username;
 
-    useEffect(getDishes, [dishes]);
+    useEffect(getDishes, [username]);
 
     function getDishes() {
         retrieveDishes()
-        .then((dishes) => {
-            setDishes(dishes.data)
+        .then((foundDishes) => {
+            setDishes(foundDishes.data)
         })
         .catch((error)=>{
             console.log(error)
@@ -33,36 +34,54 @@ export default function ListDishes() {
     }
 
     return (
-        <div>
-            <div>
-                <table className="table">
-                    <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th>Cooking time</th>
-                                <th>Servings</th>
-                                <th>Rating</th>
-                            </tr>
-                    </thead>
-                    <tbody>
-                    {
-                        dishes.map(
-                            dish => (
-                                <tr key={dish.id}>
-                                    <td>{dish.name}</td>
-                                    <td>{dish.cookingTime} min</td>
-                                    <td>{dish.servings}</td>
-                                    <td>{dish.rating}</td>
-                                    <td> <button className="btn btn-warning" 
-                                                    onClick={() => getProducts(dish.id)}>Get products</button> </td>
-                                </tr>
-                            )
-                        )
-                    }
-                    </tbody>
-
-                </table>
-            </div>
+        <div className="card-group row-cols-1 row-cols-md-2 g-4">
+            {dishes.map(
+                dish => (
+                    <div className="col">
+                        <div className="card m-5 p-2 d-flex flex-row justify-content-between align-items-center">
+                            <div className="card-body">
+                                <h5 className="card-title">{dish.name}</h5>
+                                <p className="card-text">Some description</p>
+                                <a href="#" className="btn btn-primary">Details</a>
+                            </div>
+                            <img src="..." className="card-img-top" alt="Dish photo" />
+                        </div>
+                    </div>
+                ))}
         </div>
     )
+
+    // return (
+    //     <div>
+    //         <div>
+    //             <table className="table">
+    //                 <thead>
+    //                         <tr>
+    //                             <th>Name</th>
+    //                             <th>Cooking time</th>
+    //                             <th>Servings</th>
+    //                             <th>Rating</th>
+    //                         </tr>
+    //                 </thead>
+    //                 <tbody>
+    //                 {
+    //                     dishes.map(
+    //                         dish => (
+    //                             <tr key={dish.id}>
+    //                                 <td>{dish.name}</td>
+    //                                 <td>{dish.cookingTime} min</td>
+    //                                 <td>{dish.servings}</td>
+    //                                 <td>{dish.rating}</td>
+    //                                 <td> <button className="btn btn-warning" 
+    //                                                 onClick={() => getProducts(dish.id)}>Get products</button> </td>
+    //                             </tr>
+    //                         )
+    //                     )
+    //                 }
+    //                 </tbody>
+
+    //             </table>
+        //     </div>
+        // </div>
+    // )
 }
