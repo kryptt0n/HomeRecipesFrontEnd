042e945f084bbdddb56dd52045f686b686672009
Vitaly Sukhinin <vitaliksuhinin99@gmail.com>
2024-06-30 23:54:04 -0400

    added comment section component

--- src/components/DishComponent.jsx
+++ src/components/DishComponent.jsx
@@ -382,33 +382,6 @@ export default function DishComponent() {
                 </fieldset>}
 
                 <CommentSection commentFields={commentFields} createComment={createComment} />
-
-                {/* <CommentGroup threaded>
-                <Header as='h3' dividing>
-                    Comments
-                </Header>
-                    {commentFields.map((comment, index) => (
-                        <Comment>
-                            <CommentContent>
-                                {comment.user && <CommentAuthor>{comment.user.username}</CommentAuthor>}
-                                {!comment.user && <CommentAuthor>Anonymous</CommentAuthor>}
-                                <CommentMetadata>
-                                    <div>{comment.postedDate}</div>
-                                </CommentMetadata>
-                                <CommentText>
-                                    <p>
-                                        {comment.comment}
-                                    </p>
-                                </CommentText>
-                            </CommentContent>
-                        </Comment>
-                    ))}
-
-                    <Form reply>
-                        <FormTextArea id="newComment"/>
-                        <Button content='Add Comment' type="button" labelPosition='left' icon='edit' primary onClick={createComment}/>
-                    </Form>
-                </CommentGroup> */}
                 
                 {
                     !isNewDish &&
